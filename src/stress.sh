469d0928f9709765f2c9102af28e3ff2937ce5b3
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-03-08 01:23:46 -0300

    Como escalar com o Horizontal Pod Autoscaler: Utilizando o HPA no Windows

---
+++ src/stress.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+for i in {1..10000}; do
+
+    curl localhost:30000
+    sleep $1 # intervalo em segundos recebido como parâmetro
+
+done
+
+# Executar no Git Bash, por exemplo
+    # sh stress.sh 0.001
+    # Caso queira salvar em arquivo, adicionar '> arquivo.txt' ao final do comando
